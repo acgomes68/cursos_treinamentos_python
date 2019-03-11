a358947117f136f2ba00cabaf8bcf484b21fe0d8
acgomes68 <acgomes68@gmail.com>
2019-03-11 18:47:16 -0300

    Conclusão do CRUD de livros

--- Django/djangoProj/livraria/urls.py
+++ Django/djangoProj/livraria/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('livro_list/', livro_list, name='listar_livros'),
+    path('', livro_index, name='livro_index'),
+    path('livro_list/', livro_list, name='livro_list'),
     path('livro_new/', livro_new, name='livro_new'),
+    path('livro_edit/<int:pk>', livro_edit, name='livro_edit'),
+    path('livro_remove/<int:pk>', livro_remove, name='livro_remove'),
 ]
--- Django/djangoProj/livraria/views.py
+++ Django/djangoProj/livraria/views.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -8,6 +8,9 @@ class LivroForm(ModelForm):
     class Meta:
         model = Livro
         fields = ['autor', 'editora', 'isbn', 'numeroPaginas', 'titulo', 'anoPublicacao', 'emailEditora']
+
+def livro_index(request):
+    return livro_list(request)
 
 def livro_list(request, template_name='livro_list.html'):
     livro = Livro.objects.all()
@@ -18,5 +21,23 @@ def livro_new(request, template_name='livro_form.html'):
     form = LivroForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('listar_livros')
+        return redirect('livro_index')
     return render(request, template_name, {'form': form})
+
+def livro_edit(request, pk, template_name='livro_form.html'):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == "POST":
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            return redirect('livro_index')
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, template_name, {'form': form})
+
+def livro_remove(request, pk, template_name='livro_delete.html'):
+    livro = Livro.objects.get(pk=pk)
+    if request.method == "POST":
+        livro.delete()
+        return redirect('livro_index')
+    return render(request, template_name, {'livro': livro})

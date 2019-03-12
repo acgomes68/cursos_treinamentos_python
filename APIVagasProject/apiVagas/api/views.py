e9d983436236a50cbc5b90fb8172d92149917907
acgomes68 <acgomes68@gmail.com>
2019-03-12 01:41:19 -0300

    Atualização das rotas, criação do serializer e do método de inclusão na view (controller)

--- APIVagasProject/apiVagas/api/views.py
+++ APIVagasProject/apiVagas/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
 
 # Create your views here.
+
+class VagaList(APIView):
+    def post(self, request):
+        try:
+            serializer = VagaSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

15ef00734f3d1a019c20c2852cb016c0234d804f
acgomes68 <acgomes68@gmail.com>
2019-03-12 01:20:20 -0300

    Configurações de acesso ao MySQL e execução das migrations

--- APIVagasProject/apiVagas/apiVagas/settings.py
+++ APIVagasProject/apiVagas/apiVagas/settings.py
@@ -90,6 +90,9 @@ DATABASES = {
        'PASSWORD': 'ApiVagas123',
        'HOST': 'localhost',
        'PORT': '3306',
+       'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+       },
    }
 }
 
